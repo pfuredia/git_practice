1cf6fa785de110f001e5b765c481bc4c12020e0c
pfuredia <pfuredia@gmail.com>
2021-04-08 17:39:36 +0200

    first commit

---
+++ codeacademy/mixed_messages/theCode.js
@@ -0,0 +1,4 @@
+
+let messages = ["you are aries", "you are cancer", "you are taurus ", "you are leo", "you are gemini", "you are aries", "you are gemini", "you are virgo", "you are libra", "you are capricorn", "you are scorpio", "you are aquarius", "you are saggitarius", "you are pisces"];
+let result = messages[Math.floor(Math.random()*12)];
+console.log(result);
